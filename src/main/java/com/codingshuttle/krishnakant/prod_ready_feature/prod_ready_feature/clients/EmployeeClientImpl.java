648175f556d0d7429cd9ada5a0eb219b3eab4137
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2025-08-04 18:39:33 +0530

    logs initialisation and types of logs

--- src/main/java/com/codingshuttle/krishnakant/prod_ready_feature/prod_ready_feature/clients/EmployeeClientImpl.java
+++ src/main/java/com/codingshuttle/krishnakant/prod_ready_feature/prod_ready_feature/clients/EmployeeClientImpl.java
@@ -4,7 +4,8 @@ import com.codingshuttle.krishnakant.prod_ready_feature.prod_ready_feature.advic
 import com.codingshuttle.krishnakant.prod_ready_feature.prod_ready_feature.dto.EmployeeDTO;
 import com.codingshuttle.krishnakant.prod_ready_feature.prod_ready_feature.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +20,17 @@ public class EmployeeClientImpl implements EmployeeClient{
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+
+        log.error("error log");
+        log.warn("warn log");
+        log.info("info log");
+        log.debug("debug log");
+        log.trace("trace log");
+
 
         try{
             ApiResponse<List<EmployeeDTO>> employeeDTOList = restClient.get()
